2975b9056d4b548d8c4ca1ba9ba44e58594c141f
Amazingmercy <amaefulemercy499@gmail.com>
2022-06-23 21:16:26 +0100

    A task by Zuri

---
+++ index.js
@@ -0,0 +1,6 @@
+function meInfo ( name,height, country){
+    let info=('My Personal Info Is \n Name:'+ name+'\n Height:'+ height+'cm \n Country:'+ country);
+    return console.log(info);
+   };
+   
+   meInfo('Amazingmercy', 171.3,'Nigeria');
